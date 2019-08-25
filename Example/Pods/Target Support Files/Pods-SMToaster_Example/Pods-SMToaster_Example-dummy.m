15e33e510fcb280a4a6b10859212c22291c06df1
Sharkes Monken <sharkesm@gmail.com>
2019-08-26 00:18:57 +0800

    Remove ReplaceMe.swift file

---
+++ Example/Pods/Target Support Files/Pods-SMToaster_Example/Pods-SMToaster_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SMToaster_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SMToaster_Example
+@end
---
+++ Example/Pods/Target Support Files/SMToaster/SMToaster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SMToaster : NSObject
+@end
+@implementation PodsDummy_SMToaster
+@end
